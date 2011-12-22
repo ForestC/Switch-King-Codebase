d6e3342954887962313a52f053fad6d8d07b7c29
Martin Videfors <martin@Martins-MacBook-Pro.local>
2011-12-22 14:58:34 +0100

    -

---
+++ SK Codebase/Constants.h
@@ -0,0 +1,21 @@
+//
+//  Constants.h
+//  SK Codebase
+//
+//  Created by Martin Videfors on 2011-12-07.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#ifndef SK_Codebase_Constants_h
+#define SK_Codebase_Constants_h
+
+#define XML_ELEMENT_NAME__DEVICE_ARRAY @"ArrayOfRESTDevice"
+#define XML_ELEMENT_NAME__DEVICE @"RESTDevice"
+
+#define XML_ELEMENT_NAME__DATASOURCE_ARRAY @"ArrayOfRESTDataSource"
+#define XML_ELEMENT_NAME__DATASOURCE @"RESTDataSource"
+
+#define ACTION_ID__TURN_ON 2
+#define ACTION_ID__TURN_OFF 1
+
+#endif
